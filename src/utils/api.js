757cfa0227f66be30e30e6cc6b6e4bc5103fe1d6
Dilshan Upasena <98dilshanup@gmail.com>
2025-10-01 17:08:11 +0530

    after ssl certified

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000',
-  baseURL: 'http://ec2-13-61-22-147.eu-north-1.compute.amazonaws.com:5000',
+  // baseURL: 'http://ec2-13-61-22-147.eu-north-1.compute.amazonaws.com:5000',
+  baseURL: 'https://trekbiz.duckdns.org',
   headers: {
     'Content-Type': 'application/json',
   },
