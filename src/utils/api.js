11943b7c413ca85678c5cfa442b7883bbe41d21d
Dilshan Upasena <98dilshanup@gmail.com>
2025-10-06 04:18:37 +0530

    printer connected, receipt design, product delete button fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
   // baseURL: 'http://ec2-13-61-22-147.eu-north-1.compute.amazonaws.com:5000',
-  // baseURL: 'https://trekbiz.duckdns.org',
+  baseURL: 'https://trekbiz.duckdns.org',
   headers: {
     'Content-Type': 'application/json',
   },
